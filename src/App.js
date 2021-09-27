f16c2f6256b3c19a84903dd464b78a551edde25b
Abhishek Gaur <abhishekgaur@Abhisheks-Mini.attlocal.net>
2021-09-27 11:58:49 -0700

    update app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-
-
-
-
 function getAuth() {
-  console.log('aaa');
 
   const data =  {
   grant_type:'authorization_code',
@@ -26,48 +21,18 @@ function getAuth() {
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'
   };
 
-
   axios.post('https://authorization-server.com/token', data, {headers})
     .then(res => {
       console.log(res);
       
     })
-
-
-    // do authorize
-
-// var data  = {
-//   response_type:'code',
-// client_id:'7Aub1XP8gKBvfnQjTfGhLWj6',
-// redirect_uri:'https://www.oauth.com/playground/authorization-code.html',
-// scope:'photo+offline_access',
-// state:'-rV4mUSJDe-w0GWH',
-//   };
-
-
-//   var headers = {
-//     'Content-Type': 'text/html',
-//     'Access-Control-Allow-Origin': '*',
-//     'Accept': 'text/html',
-//     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-//     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'
-//   };
-
-
-//   axios.post('https://authorization-server.com/authorize', data, {headers})
-//     .then(res => {
-//       console.log(res);
-      
-//     })
-
-
 }
 
 function App() {
   return (
     <div className="App">
       <div class="click-here">
-        <button onClick={getAuth}>Click</button>
+        <button onClick={getAuth}>Authorize</button>
       </div>
       {/* <header className="App-header">
 
